dea50c65de5e8a5e9fe6360e36328febb7031ec1
choise <fate.feb@gmail.com>
2022-03-17 14:35:26 +0900

    studied contents

---
+++ README.md
@@ -0,0 +1,13 @@
+# Basic Babel
+
+#### updating studied source codes
+
+------------
+#### Contents
+- set babel-loader (add in webpack.config.js)
+- about sass(using .scss not .sass)
+- set sass-loader to use scss (add in webpack.config.js)
+
+------------
+###### watched inflearn lecture :)
+###### https://www.inflearn.com/course/%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%94%EB%93%9C-%EA%B0%9C%EB%B0%9C%ED%99%98%EA%B2%BD/dashboard
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          /**
+           * TODO: SASS 코드를 사용할수 있겠끔 sass-loader를 구성하세요.
+           */
+          
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader // 프로덕션 환경
+            : "style-loader", // 개발 환경
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif|jpeg)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 10000 // 10Kb
+        }
+      },
+      /**
+       * TODO: babel-loader를 구성해 보세요.
+       */
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true // 주석 제거
+            }
+          : false,
+      hash: process.env.NODE_ENV === "production"
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production" ? [new MiniCssExtractPlugin({ filename: `[name].css` })] : [])
+  ]
+};
